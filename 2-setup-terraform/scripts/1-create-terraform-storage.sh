8306a043af012962977967189c0b4d0639db52d8
voyagian <98567340+voyagian@users.noreply.github.com>
2023-05-25 05:21:05 -1000

    Update RG and SA names and location

--- 2-setup-terraform/scripts/1-create-terraform-storage.sh
+++ 2-setup-terraform/scripts/1-create-terraform-storage.sh
@@ -3,14 +3,14 @@
 
 # Creates the relevant storage account to store terraform state locally
 
-RESOURCE_GROUP_NAME="deploy-first-containerapp-rg"
-STORAGE_ACCOUNT_NAME="deployfirstcontainersa"
+RESOURCE_GROUP_NAME="voyagian-deploy-first-containerapp-rg"
+STORAGE_ACCOUNT_NAME="vgiandeployfirstcntersa"
 
 # Create Resource Group
-az group create -l uksouth -n $RESOURCE_GROUP_NAME
+az group create -l westus -n $RESOURCE_GROUP_NAME
 
 # Create Storage Account
-az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l uksouth --sku Standard_LRS
+az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l westus --sku Standard_LRS
 
 # Create Storage Account blob
 az storage container create  --name tfstate --account-name $STORAGE_ACCOUNT_NAME
